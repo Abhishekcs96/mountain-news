dee70d3e4cee197bcaa4af289f59509b3cb344f6
Abhishekcs96 <abhishekcs96@gmail.com>
2023-10-07 23:26:08 +1100

    working first draft

--- fetch-news.sh
+++ fetch-news.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
 
-# Hardcode it all the first time
-
-MOUNTAIN_NEWS="https://explorersweb.com/category/mountaineering/"
-
-COMMAND="2>/dev/null curl https://explorersweb.com/category/mountaineering/ \
+URL_FOR_NEWS="https://explorersweb.com/category/mountaineering/"
+PATTERN="${URL_FOR_NEWS%*/*/*/}"
+#echo "${PATTERN}"
+COMMAND="$(2>/dev/null curl "${URL_FOR_NEWS}" \
     | pandoc -t markdown | grep -A1 -i -e '<div class="news-card">' \
-    | grep -v -e'<div class="news-card">' | grep -e "https://explorersweb.com/" \
-    | awk -F\" '{print $2}' | head -1 | xargs -I_ curl _ 2>/dev/null \
-    | grep --only-matching -e'<p>.*</p>' | ghead -n -3 | pandoc -f html -t markdown"
+    | grep -v -e'<div class="news-card">' | grep -e "${PATTERN}" \
+    | awk -F\" '{print $2}')" 
+
+RANDOM_NUMBER="${RANDOM}" 
+
+#display_news(){
+declare -i COUNT
+COUNT=1
+while read -r LINE; do
+    echo -e ""${COUNT}"\t"${LINE}"" | tee -a "/tmp/news-${RANDOM_NUMBER}"
+    COUNT=$COUNT+1
+done < <(echo "${COMMAND}")
+#}
+
+#pick_news(){
+NUMBER_PATTERN_1="^[0-9]$"
+NUMBER_PATTERN_2="^[0-9]{2}$"
+#[ -t 0 ] || exec </dev/tty # reattach keyboard to STDIN
+read -p "Pick a number, dummy " -n 2 -r
+echo
+[[ $REPLY =~ ${NUMBER_PATTERN_1} ]] || [[ $REPLY =~ ${NUMBER_PATTERN_2} ]] || echo "Pattern not matched...exiting"
+cat "/tmp/news-${RANDOM_NUMBER}" | head -n "$REPLY" | tail -n 1 | awk -F'\t' '{print $NF}' | xargs -I{} -- 2>/dev/null curl {} | \
+    grep --only-matching -e '<p>.*</p>' | ghead -n -3 | pandoc -f html -t markdown | less || \
+    echo "Cannot find number entered idiot...hahaha"
+
+rm "/tmp/news-${RANDOM_NUMBER}"
