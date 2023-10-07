e1adcb467fe3431d5a9ca5ab45c11cd68037f8e9
Abhishekcs96 <abhishekcs96@gmail.com>
2023-10-07 22:22:36 +1100

    Tidy up

--- fetch-news.sh
+++ fetch-news.sh
@@ -5,4 +5,8 @@
 
 MOUNTAIN_NEWS="https://explorersweb.com/category/mountaineering/"
 
-command="2>/dev/null curl https://explorersweb.com/category/mountaineering/ | pandoc -t markdown | grep -A1 -i -e '<div class="news-card">' | grep -v -e'<div class="news-card">' | grep -e "https://explorersweb.com/" | awk -F\" '{print $2}' | head -1 | xargs -I_ curl _ 2>/dev/null | grep --only-matching -e'<p>.*</p>' | ghead -n -3 | pandoc -f html -t markdown"
+COMMAND="2>/dev/null curl https://explorersweb.com/category/mountaineering/ \
+    | pandoc -t markdown | grep -A1 -i -e '<div class="news-card">' \
+    | grep -v -e'<div class="news-card">' | grep -e "https://explorersweb.com/" \
+    | awk -F\" '{print $2}' | head -1 | xargs -I_ curl _ 2>/dev/null \
+    | grep --only-matching -e'<p>.*</p>' | ghead -n -3 | pandoc -f html -t markdown"
