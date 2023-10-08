d6aced2ae342d051916f0376721e101eceaadc64
Abhishekcs96 <abhishekcs96@gmail.com>
2023-10-08 23:36:25 +1100

    Adding more stuff

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 I like doing stuff on my terminal which includes reading news.
 
-This is a simple (shoddy) script that fetches news from [explorer web's mountaineering page](https://explorersweb.com/category/mountaineering/) since I like to read mountaineering news.
+This is a simple (shoddy) script that fetches news from [explorer web's mountaineering page](https://explorersweb.com/category/) since I like to read news re. a bunch of topics involving outdoorsy,alpinistic and mountaineery stuff.
+
+You can choose from the categories listed, currently it supports only 5 categories of news.
 
 ***NOTE:*** This script does use the BSD and GNU variant of head and possibly other commands...which I probably need to modify at some point...
+
+## Usage
+
+- Clone this repo
+- You can alias this script if you want, add it to your relevant rc file.
+- run `./fetch-news.sh -h` to see all the categories you can read news from.
--- fetch-news.sh
+++ fetch-news.sh
@@ -2,18 +2,74 @@
 
 set -eu -o pipefail
 
-URL_FOR_NEWS="https://explorersweb.com/category/mountaineering/"
+show_help(){
+    echo  "USAGE: $0 [-c value] [--help]"
+    echo  "OPTIONS:"
+    echo -e " -c       catergory of news (default is mountaineering), choose from:\n"
+    echo -e "\t\tmountaineering\n\t\texpeditions\n\t\tpolar-exploration\n\t\tlong-distance-trekking\n\t\t8000ers"
+    echo -e "\n--help   Display this help message"
+    exit 1
+}
+
+DEFAULT_CATEGORY="mountaineering"
+
+while getopts ":c:h" opt; do
+    case $opt in
+        c)
+            DEFAULT_CATEGORY="${OPTARG}"
+            ;;
+        h|--help)
+            show_help 
+            ;;
+        \?)
+            echo "Invalid option: ${OPTARG}" >&2 
+            exit 1
+            ;;
+        :)
+            echo "Option -${OPTARG} requires an argument" >&2 
+            ;;
+    esac
+done   
+
+LOWER_DEFAULT_CATEGORY="${DEFAULT_CATEGORY,,}"
+
+case $LOWER_DEFAULT_CATEGORY in
+    mountaineering)
+        :
+        ;;
+    expeditions)
+        :
+        ;;
+    polar-exploration)
+        :
+        ;;
+    long-distance-trekking)
+        :
+        ;;
+    8000ers)
+        :
+        ;;
+    *)
+        echo "Invalide choice...Exiting"
+        exit 1
+        ;;
+esac
+
+echo "Checking for news in category: ${LOWER_DEFAULT_CATEGORY}..."
+echo
+
+RANDOM_NUMBER="${RANDOM}" 
+URL_FOR_NEWS="https://explorersweb.com/category/${LOWER_DEFAULT_CATEGORY}/"
 PATTERN="${URL_FOR_NEWS%*/*/*/}"
 COMMAND="$(2>/dev/null curl "${URL_FOR_NEWS}" \
     | pandoc -t markdown | grep -A1 -i -e '<div class="news-card">' \
     | grep -v -e'<div class="news-card">' | grep -e "${PATTERN}" \
     | awk -F\" '{print $2}')" 
-RANDOM_NUMBER="${RANDOM}" 
 
 pick_news(){
     NUMBER_PATTERN_1="^[0-9]$"
     NUMBER_PATTERN_2="^[0-9]{2}$"
-    read -p "Pick a number " -n 2 -r
+    read -p "Enter the number for the article you would like to read " -n 2 -r
     echo
     # Check if it matches pattern , then continue next else just exit out
     [[ $REPLY =~ ${NUMBER_PATTERN_1} ]] || [[ $REPLY =~ ${NUMBER_PATTERN_2} ]] || echo "Pattern not matched..."
