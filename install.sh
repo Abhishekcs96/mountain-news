1e858a5512639e2e4a9db7eab9af63838009a9bd
Abhishekcs96 <abhishekcs96@gmail.com>
2023-10-12 09:12:45 +1100

    more stuff in installation

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+# TODO: Better logging
+# TODO: Check if directories available or not and if the binaries are installed
+# TODO: eg: if chmod exisits, if cp exists and if /usr/local/bin exists...
+echo "Setting up..."
 sudo cp ./news /usr/local/bin/ 
 sudo chmod +x /usr/local/bin/news
+if [[ $? ]]; then
+    echo "Setup complete..."
+    echo "Type in \"news\" and see what happens..."
+else
+    1>&2 echo "setup not complete, error encountered..."
+fi
